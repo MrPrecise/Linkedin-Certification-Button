43bee5d37ffecd59034614a9cdfd837df871fe68
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-07-13 14:13:14 +0200

    Create Coursera.js
    Creation of the script

---
+++ Scripts/Coursera.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Coursera certification button
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Adds a button on your coursera accomplishments page for spesific certifications. This button when clicked takes information from the site and adds it to the Linkedin Certification
+// @author       Eirik Thue
+// @match        https://www.coursera.org/account/accomplishments/verify/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=coursera.org
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // Your code here...
+})();
