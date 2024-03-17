7a8e767a6e402e6b91cf6e4df5b1d166a6ab3238
MrPrecise <MrPrecise@users.noreply.github.com>
2024-03-17 19:45:58 +0100

    Update Real-Python.js

--- Scripts/Real-Python.js
+++ Scripts/Real-Python.js
@@ -11,13 +11,11 @@
 
 (function () {
   /**
-   * Making sure the whole page is loaded before using the script by running the window.onload()
-   * This ensures no information is not lost in the transfer
+   * Making sure everything is loaded before using the script
    */
   window.onload = (event) => {
     /**
      * Select the date on the Real Python webpage and select the <p> with "<strong>Completion Date:</strong> Month DD, Year"
-     * The query selecter select the div containing the date
      */
     let date = document.querySelector(
       "body > div.container.main-content > div > div.col-12.my-0.noselect > figure > div > div.w-100.d-flex > div > div > p.mb-0"
@@ -25,7 +23,6 @@
 
     /**
      * Remove the excess "Completion Date" HTML and save it as "Month DD, Year" format as string
-     * The query selecter select the div containing the name of the course
      */
     date = date.split("<strong>Completion Date:</strong> ").findLast((y) => y);
 
@@ -37,27 +34,23 @@
     ).innerHTML;
 
     /**
-     * Store the start of the link in a variable where you can build the data extending the URL
+     *
      */
     const addCertificationUrl =
       "https://www.linkedin.com/profile/add?startTask=CERTIFICATION_NAME&";
 
     /**
      * Save name=coursename& as string for the final query
-     * The final string is extending the addCertificationUrl in the addToLinkedIn() to complete the link to transfer the data contianing the coursename
      */
     const name = "name=" + courseName + "&";
 
     /**
      * Save organizationId=3130543& as string (3130543 Is Real Python Orginization ID on LinkedIn)
-     * The final string is extending the addCertificationUrl in the addToLinkedIn() to complete the link to transfer the data contianing the
-     * Orginization ID of Real Python on LinkedIn
      */
     const organizationId = "organizationId=" + 3130543 + "&";
 
     /**
      * Save issueYear= and year parsed in by date .getFullYear() will return only the year, and save it  as string for the final query
-     * The final string is extending the addCertificationUrl in the addToLinkedIn() to complete the link to transfer the data contianing the Year of Compleation
      */
     const issueYear =
       "issueYear=" + new Date(Date.parse(date)).getFullYear() + "&";
@@ -65,7 +58,6 @@
     /**
      * Save issueYear= and month parsed in by date .getMonth() will return only the month, and save it  as string for the final query
      * getMonth() gives number from 0 to 11 hence the + 1 after getMonth()
-     * The final string is extending the addCertificationUrl in the addToLinkedIn() to complete the link to transfer the data contianing the Month of Completion
      */
     const issueMonth =
       "issueMonth=" + (new Date(Date.parse(date)).getMonth() + 1) + "&";
@@ -74,14 +66,12 @@
      * Save certId= and the certification ID, then save it as string for the final query
      * "window.location.href.split("/").findLast((y) => y" Splits the URL and keep the last part which would be the ID
      * Example: https://realpython.com/certificates/123456789abcd => 123456789abcd
-     * The final string is extending the addCertificationUrl in the addToLinkedIn() to complete the link to transfer the data contianing the Certification ID
      */
     const certId =
       "certId=" + window.location.href.split("/").findLast((y) => y) + "&";
 
     /**
      * Save certUrl=url& as string for the final query
-     * The final string is extending the addCertificationUrl in the addToLinkedIn() to complete the link to transfer the data contianing the URL To the Certificate
      */
     const certUrl = "certUrl=" + window.location.href;
 
@@ -92,19 +82,23 @@
       "body > div.container.main-content > div > div.col-md-11.col-lg-8.article > div.text-center.my-3.d-print-none > div > span"
     );
 
-    /**
+     /**
+     * Creating new button
+     */
+     var newButton = '<a id="newLinkedIn" class="mr-1 btn btn-sm badge-linkedin text-light mb-1"><span class="icon baseline mr-1 text-light"><svg><use href="/static/icons.06c57406e4fc.svg#brands--linkedin"></use></svg></span>Add Cert to LinkedIn</a>'
+
+     /**
      * Adds our own button to the collection and link it to our addToLinkedIn() function
      */
     buttonSection.insertAdjacentHTML(
-      "afterbegin",
-      `<a class="mr-1 btn btn-sm badge-linkedin text-light mb-1"><i class="mr-1 fa fa-linkedin text-light"></i>Add to LinkedIn</a>`
+      "beforebegin",
+      newButton
     );
-    buttonSection.onclick = function () {
-      addToLinkedIn();
-    };
+
+    document.getElementById('newLinkedIn').addEventListener("click", addToLinkedIn)
 
     /**
-     * Opens the LinkedIn in new tab with your certification information filled into the input field in
+     * Opens the LinkedIn in new tab with your certification information filled in
      */
     function addToLinkedIn() {
       window.open(
